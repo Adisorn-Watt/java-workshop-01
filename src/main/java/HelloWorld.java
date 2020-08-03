53828cdad80d4d15f0a1ba4d3875a433e48fd3f4
Adisorn Wattana <adisorn.wattana@allianz.com>
2020-08-03 12:21:18 +0700

    [Update] delete redundant code

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 
     public String sayHi(String name) {
         String firstLetter = name.substring(0, 1).toUpperCase();
-        String otherLetter = name.substring(1, name.length());
+        String otherLetter = name.substring(1);
         String capitalizedFirstLetterName = firstLetter + otherLetter;
         return "Hello " + capitalizedFirstLetterName;
     }
